c2365f62937cf790aeae9b1369e872d206b2ce92
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-04-25 20:26:49 +0500

    learning variables var, let, const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456;
+let accountEmail = "zk3762@gmail.com";
+var accountPassword = "zeeshan";
+accountCity = "karachi";
+let account;
+
+// accountId = 12;
+
+accountEmail = "zk@gmail.com";
+accountPassword = "123";
+accountCity = "lahor";
+console.table([accountEmail, accountPassword, accountCity, account]);
